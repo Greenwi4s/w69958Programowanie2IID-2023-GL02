7724b6d55af81b9c9a1ef534915cabc62ed8203d
Greenwi4s <64439718+Greenwi4s@users.noreply.github.com>
2024-05-10 11:30:36 +0200

    sd

--- LAB 7 - 8/script.js
+++ LAB 7 - 8/script.js
@@ -26,3 +26,67 @@ function sprawdzDateUrodzenia() {
     dataUrodzenia.addEventListener('input', () => {
     sprawdzDateUrodzenia()
 });
+//Funkcja sprawdzająca czy pole jest puste. Gdy jest puste ustawia treść wiadomości w polu z błędem oraz zwraca true
+function requiredValidation(field, errorField) {
+  //filed.value zawiera wartość wprowadzoną w polu
+  //!field.value sprawdza czy pole nie jest nullem albo undefined
+  if (!field.value || field.value === '') {
+      errorField.innerHTML = 'To pole jest wymagane';
+      return true;
+  }
+  else {
+      errorField.innerHTML = '';
+      return false;
+  }
+}
+
+//Funkcja sprawdzająca czy pole spełnia wymaganie co do ilości znaków
+//Funkcja przyjmuje parametr minLength (domyślnie ustawiany na 0), który definuje ile znaków jest wymaganych w polu
+function minLengthValidation(field, errorField, minLength = 0) {
+  if (field.value.length < minLength) {
+      errorField.innerHTML = `To pole musi mieć conajmniej ${minLength} znaków`;
+      return true;
+  }
+  else {
+      errorField.innerHTML = '';
+      return false;
+  }
+}
+
+
+
+//Funckja sprawdzająca czy któreś pole typu radio zostało zaznaczone
+function radioRequiredValidation(fields, errorField, errorMessage = '') {
+  let result = false;
+
+  fields.forEach(field => {
+      result = field.checked || result;
+  });
+
+  errorField.innerHTML = result ? '' : errorMessage;
+
+  return !result;
+}
+
+
+
+
+function validConfirmPassword(confirmPasswordField, passwordField, errorField) {
+  if (confirmPasswordField.value !== passwordField.value) {
+      errorField.innerHTML = 'Hasła nie są takie same';
+      return true;
+  }
+  else {
+      errorField.innerHTML = '';
+      return false;
+  }
+}
+const telefon = document.getElementById('telefon');
+
+telefon.addEventListener('keydown', (event) => {
+  // Sprawdź, czy naciśnięty klawisz nie jest cyfrą (0-9)
+  if (!/[0-9]/.test(event.key)) {
+    // Zapobiegnij domyślnemu zachowaniu (wpisanie znaku)
+    event.preventDefault();
+  }
+});
